8e34a446ab21e50635fe62a1d608e57f6e94c399
Niels Drost <nielsdrost7@gmail.com>
2016-10-23 09:24:45 +0200

    Relations adminmaster layout

---
+++ Modules/Relations/Resources/Views/layouts/adminmaster.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Relations Admin Master {{ config('app.short_name') }} | {{ $page_title or "Page Title" }}</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, staff-scalable=no' name='viewport'>
+    <!-- Set a meta reference to the CSRF token for use in AJAX request -->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <!-- Bootstrap 3.3.4 -->
+    <link href="{{ asset("/bower_components/admin-lte/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome Icons 4.4.0 -->
+    <link href="{{ asset("/bower_components/admin-lte/font-awesome/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css" />
+    <!-- Ionicons 2.0.1 -->
+    <link href="{{ asset("/bower_components/admin-lte/ionicons/css/ionicons.min.css") }}" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="{{ asset("/bower_components/admin-lte/dist/css/AdminLTE.min.css") }}" rel="stylesheet" type="text/css" />
+
+
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,300|Open+Sans:400,600,700,800' rel='stylesheet'
+          type='text/css'>
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css' rel='stylesheet'
+          type='text/css'>
+
+
+
+    <!-- Application CSS-->
+    <link href="{{ asset(elixir('css/all.css')) }}" rel="stylesheet" type="text/css" />
+
+    <!-- Head -->
+    @include('partials._head')
+
+      <!-- REQUIRED JS SCRIPTS -->
+
+
+    <link href="http://faveohelpdesk.local/lb-faveo/plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" >
+
+    <script src="http://faveohelpdesk.local/lb-faveo/js/jquery-2.1.4.js" type="text/javascript"></script>
+    <script src="http://faveohelpdesk.local/lb-faveo/js/jquery2.1.1.min.js" type="text/javascript"></script>
+
+
+      <!-- Optionally, you can add Slimscroll and FastClick plugins.
+            Both of these plugins are recommended to enhance the
+            user experience. Slimscroll is required when using the
+            fixed layout. -->
+
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+      <!-- Application JS-->
+      <script src="{{ asset(elixir('js/all.js')) }}"></script>
+
+      <!-- Optional header section  -->
+      @yield('head_extra')
+
+  </head>
+
+  <!-- Body -->
+  @include('core::partials._body')
+
+</html>
